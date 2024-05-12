c6aa5135cb6f5ed87ffd6875d4763990b7b8b405
xionkq <3030199850@qq.com>
2024-05-12 22:27:20 +0800

    feat: show post by slug

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,17 +1,45 @@
 import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
+import path from 'node:path'
+
+const ERROR_MESSAGE_NOT_FOUND = 'error not found'
+
+interface PostMetaData {
+  title?: string
+  date?: Date
+  updated?: string
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const data = matter.read('./app/blog/(posts)/framework-and-pages.md')
+  const post = await getPostInfoBySlug(params.slug)
 
-  const processedContent = await remark().use(html).process(data.content)
-  const contentHtml = processedContent.toString()
+  if (post === ERROR_MESSAGE_NOT_FOUND) {
+    return <div>404</div>
+  }
+
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: contentHtml,
-      }}
-    ></div>
+    <>
+      <h2>{post.metaData.title}</h2>
+      <p>{post.metaData.date?.toString()}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+    </>
   )
+}
+
+async function getPostInfoBySlug(slug: string) {
+  try {
+    const postsDirectory = './app/blog/(posts)'
+    const fullPath = path.join(postsDirectory, `${slug}.md`)
+    const matterResult = matter.read(fullPath)
+
+    const processedContent = await remark().use(html).process(matterResult.content)
+    return {
+      metaData: matterResult.data as PostMetaData,
+      content: processedContent.toString(),
+    }
+  } catch (err) {
+    console.error(err)
+    return ERROR_MESSAGE_NOT_FOUND
+  }
 }
